47e1ffe0e30c50caf89efd6340d5449c5d38c5a6
Valentina <valya.cherednichenko.03@mail.ru>
2023-01-28 04:33:06 +0300

    arguments and options

--- bin/gendiff.js
+++ bin/gendiff.js
@@ -3,7 +3,10 @@ import { Command } from 'commander';
 const program = new Command();
 
 program
+  .version('0.1.0')
   .description('Compares two configuration files and shows a difference.')
-  .option('-V, --version', 'output the version number')
+  .option('-f, --format <type>',  'output format')
+  .argument('<filepath1>')
+  .argument('<filepath2>')
 
 program.parse();
