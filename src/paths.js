c7b82a582015e7b36624657039e51e5fc5fede32
Valentina <valya.cherednichenko.03@mail.ru>
2023-01-31 21:51:58 +0300

    add extname

--- src/paths.js
+++ src/paths.js
@@ -2,4 +2,7 @@ import { cwd } from 'node:process';
 import path from 'node:path';
 
 const paths = (filepath) => (path.isAbsolute(filepath) ? filepath : path.resolve(cwd(), '__fixtures__', filepath));
+
+export const extname = (filepath) => path.extname(filepath);
+
 export default paths;
